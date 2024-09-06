afb92fe9fe600191d979db62de29b771867a22cf
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2024-09-06 18:19:12 +0700

    add: package json

--- index.js
+++ index.js
@@ -1,8 +1,58 @@
-const express = require("express");
-const app = express();
+const Express = require('express')
+const App = Express()
+const Cors = require('cors')
+const Morgan = require('morgan')
+const Dotenv = require('dotenv')
+const Helmet = require('helmet')
+const Session = require('express-session')
+const Path = require('path')
+const HOST = '0.0.0.0'
+const CookieParser = require('cookie-parser')
+const HealthCheck = require('express-healthcheck')
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+App.use(Helmet())
+App.use(Cors()) 
+App.use(Express.json())
+App.use(Express.urlencoded({ extended: false }))
+App.use(Express.static('public'))
+App.set('view engine', 'ejs')
+App.use(Morgan('dev'))
+App.use(CookieParser())
+App.use(Session({
+    secret: process.env.SecretKey || 'secret no rumpi',
+    resave: false,
+    saveUninitialized: false,
+}))
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+App.get("/", (req, res) => res.send("Express on Vercel"));
 
-module.exports = app
+App.listen(3000, () => console.log("Server ready on port 3000."));
+
+let startTime = Date.now()
+App.use('/v1/health-check', HealthCheck(
+    {
+        healthy: () => {
+            function formatUptime(uptimeInSeconds) {
+                const hours = Math.floor(uptimeInSeconds / 3600)
+                const minutes = Math.floor((uptimeInSeconds % 3600) / 60)
+                const seconds = Math.floor(uptimeInSeconds % 60)
+                return `${hours}h ${minutes}m ${seconds}s`
+            }
+
+            const healthyStatus = true
+            const databaseStatus = true
+            const uptimeInSeconds = Math.floor((Date.now() - startTime) / 1000)
+            const uptimeFormatted = formatUptime(uptimeInSeconds)
+
+            return {
+                healthy: healthyStatus,
+                database: databaseStatus,
+                uptime: process.uptime(),
+                uptimeFormatted: uptimeFormatted,
+                version: process.env.APP_VERSION || 'Null'
+            }
+        },
+    }
+))
+
+module.exports = App
